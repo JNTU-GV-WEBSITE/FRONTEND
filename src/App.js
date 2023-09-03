e41449a818301ac26819862afe1f3174c5fe8b01
saikiran11 <saikiranb.project@gmail.com>
2023-09-03 19:55:18 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import TopLayer from './components/TopLayer/TopLayer';
 // import Navbar from './components/navbar/Navbar';
 // import ImageGallery from './components/ImageGallery/ImageGallery';
 import BelowBar from './components/BelowBar/BelowBar';
+import Dropdown from './components/Navbars/Dropdown';
+import {Routes,Route} from 'react-router-dom';
+
+import Notifications from './components/Notifications/Notifications';
+
+
 
 function App() {
   return (
-    <div><Navbar/>
-    <BelowBar/></div>
+    <div>
+      {/* <Navbar/> */}
+    <BelowBar/>
+    <Dropdown/>
+    <Routes>
+      <Route path='/' element={<BelowBar/>}/>
+    <Route path='/Main' element={<BelowBar/>}/>
+  </Routes>
+    </div>
     
   );
 }
