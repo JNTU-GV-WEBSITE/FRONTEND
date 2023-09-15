855949701015d175dd81769869777970e0cbd338
sumasribhogi08 <143118822+sumasribhogi08@users.noreply.github.com>
2023-09-15 20:23:00 +0530

    updated files created by Sumasri and Gayathri

---
+++ src/components/MainPage/Header/Directorates/FacultyDevelopmentCell.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import '../Administration/AdminandDirectorates.css'; // Import your CSS file here
+import { Link, useLocation } from 'react-router-dom';
+function FacultyDevelopmentCell() {
+  const location = useLocation(); 
+  return (
+    <div className="mainContainer">
+      <div className="allLeftMenu">
+        {/* Navigation links for Chancellor, Vice Chancellor, and Registrar */}
+        <Link
+          to="/AcademicAudit"
+          className={`menuButton ${location.pathname === '/AcademicAudit' ? 'active' : ''}`}
+        >
+          Director of Academic Audit
+        </Link>
+        <Link
+          to="/AcademicPlanning"
+          className={`menuButton ${location.pathname === '/AcademicPlanning' ? 'active' : ''}`}
+        >
+          Director of Academic Planning
+        </Link>
+        <Link
+          to="/Admissions"
+          className={`menuButton ${location.pathname === '/Admissions' ? 'active' : ''}`}
+        >
+          Director of Admissions
+        </Link>
+        <Link
+          to="/AlumniRelations"
+          className={`menuButton ${location.pathname === '/AlumniRelations' ? 'active' : ''}`}
+        >
+          Director of Alumni Relations
+        </Link>
+        <Link to="/Empowerment" className={`menuButton ${location.pathname === '/Empowerment' ? 'active' : ''}`}>Empowerment of Women & Grievances</Link>
+        <Link to="/Evaluation" className={`menuButton ${location.pathname === '/Evaluation' ? 'active' : ''}`}>Evaluation</Link>
+        <Link to="/FacultyDevelopmentCell" className={`menuButton ${location.pathname === '/FacultyDevelopmentCell' ? 'active' : ''}`}>Faculty Development Cell</Link>
+        <Link to="/ForeignUniversityRelations" className={`menuButton ${location.pathname === '/ForeignUniversityRelations' ? 'active' : ''}`}>Foreign University Relations</Link>
+        <Link
+          to="/IQAC"
+          className={`menuButton ${location.pathname === '/IQAC' ? 'active' : ''}`}
+        >
+          Director of IQAC
+        </Link>
+        <Link
+          to="/Placements"
+          className={`menuButton ${location.pathname === '/Placements' ? 'active' : ''}`}
+        >
+          Director of IIIT & Placements
+        </Link>
+        <Link
+          to="/Research"
+          className={`menuButton ${location.pathname === '/Research' ? 'active' : ''}`}
+        >
+          Director of Research & Development
+        </Link>
+        <Link
+          to="/SportsAdministration"
+          className={`menuButton ${location.pathname === '/SportsAdministration' ? 'active' : ''}`}
+        >
+          Director of University Games & Sports Administration
+        </Link>
+      </div>
+      <div className="allRightContent">
+        <div className="allRightContentHeading">
+          Director of Faculty Development Cell
+        </div><br></br>
+        <div className="allRightContentProfile">
+          {/* Profile information */}
+          <center>
+          <div className="profileImage">
+            
+<img src="https://jntugv.edu.in/images/9266a0f9fc226ffbf57a3beb4c6b6388/profilephotos/registrar.jpeg" alt="Profile" />
+        
+          </div>
+          <div className="profileName">
+            <h3>Dr.G.Jaya Suma</h3>
+            <h3>Director of Faculty Development Cell</h3>
+          </div>
+          <div className="profileDesignation">
+            <h4 className="regularStyleH4">Professor of Dept. of Information Technology</h4>
+          </div>
+          </center>
+          <hr />
+          {/* Profile description */}
+          <p>Email: dfdp@jntugv.edu.in</p><br></br>
+
+<p>Dr. G. Jaya Suma, present working as a Professor in Department of Information Technology, Registrar, Director (i/c) of Industry Institute Interaction Placements & Training & Director(i/c) of Faculty Development Cell and Executive Council member of JNTUGV - Vizianagaram, AP. Completed her B.Tech (CS&SE), MTech (CS&T), Ph.D(CSE). Having experience of 22 plus years Teaching, Research and Administration of Engineering Education</p>
+
+<p className='a'>Administration Contributions</p>
+<p>❖	Served as Head, Department of Information Technology, university college of Engineering, since 2014 to August 2022.</p>
+<p>❖    Special Officer of New Engineering College, JNTU-GV, Kurupam since from May 2020 to October 2022.</p>
+<p>❖	IQAC Coordinator, university college of Engineering January 2020 to September 2022.</p>
+<p>❖	SPOC of university college of Engineering for NPTEL Online Courses to till date.</p>
+<p>❖	Convener for Women Empowerment & Grievance Cell, JNTUK, University College of Engineering from September 2017 to till date.</p>
+<p>❖	Secretary & Member for Women Empowerment & Grievance Cell from January 2014-August 2017.</p>
+<p className='a'>Academic Contributions</p>
+<p>❖	Specialization of Artificial Intelligence, Data Mining, Machine Learning, Deep Learning, Soft Computing, and Internet of Things.</p>
+<p>❖    Authored Book Titled “Artificial Intelligence & Machine Learning-2022.</p>
+<p>❖	Awarded CMI Level 5 in Management and Leadership-2021.</p>
+<p>❖	Awarded Out Standing Women in Engineering-VIWA-2017.</p>
+<p>❖	Awarded Woman Researcher in Science and Technology-2020.</p>
+<p>❖	Chairperson BOS, Dept. of Information Technology. (2017-2022).</p>
+<p>❖	Member of Board of Studies for 3 years (2007-2010) in the Dept of CSE, GIT, GITAM University.</p>
+<p className='a'>Research Contributions</p>
+<p>❖	Awarded Six PhDs and Eight Scholars are working for PhD at present.</p>
+<p>❖    Published 3 patents.</p>
+<p>❖	Published 54 International Journals, 20 International Conferences, 2 National Conferences.</p>
+<p>❖	Published 5 Book chapters.</p>
+<p>❖	32 workshops Organized, 58 Attended, 5 Chaired sessions in international Conferences, and 32 technical talks delivered.</p>
+<p className='a'>Professional Contributions</p>
+<p>❖	Member of CSI (ID – 01170956).</p>
+<p>❖  Senior Fellow of IEEE-92245858.</p>
+<p>❖	Member of ISTE- (ID – LM44520).</p>
+<p>❖	Faculty sponsor of IEEE WIE and faculty sponsor of ACM CHAPTER.</p>
+ 
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FacultyDevelopmentCell;
---
+++ src/components/MainPage/Header/Directorates/Research.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import '../Administration/AdminandDirectorates.css'; // Import your CSS file here
+import { Link, useLocation } from 'react-router-dom'; 
+
+function Research(){
+    const location = useLocation(); 
+  return (
+    <div className="mainContainer">
+      <div className="allLeftMenu">
+      <Link
+          to="/AcademicAudit"
+          className={`menuButton ${location.pathname === '/AcademicAudit' ? 'active' : ''}`}
+        >
+          Director of Academic Audit
+        </Link>
+        <Link
+          to="/AcademicPlanning"
+          className={`menuButton ${location.pathname === '/AcademicPlanning' ? 'active' : ''}`}
+        >
+          Director of Academic Planning
+        </Link>
+        <Link
+          to="/Admissions"
+          className={`menuButton ${location.pathname === '/Admissions' ? 'active' : ''}`}
+        >
+          Director of Admissions
+        </Link>
+        <Link
+          to="/AlumniRelations"
+          className={`menuButton ${location.pathname === '/AlumniRelations' ? 'active' : ''}`}
+        >
+          Director of Alumni Relations
+        </Link>
+        <Link to="/Empowerment" className={`menuButton ${location.pathname === '/Empowerment' ? 'active' : ''}`}>Empowerment of Women & Grievances</Link>
+        <Link to="/Evaluation" className={`menuButton ${location.pathname === '/Evaluation' ? 'active' : ''}`}>Evaluation</Link>
+        <Link to="/FacultyDevelopmentCell" className={`menuButton ${location.pathname === '/FacultyDevelopmentCell' ? 'active' : ''}`}>Faculty Development Cell</Link>
+        <Link to="/ForeignUniversityRelations" className={`menuButton ${location.pathname === '/ForeignUniversityRelations' ? 'active' : ''}`}>Foreign University Relations</Link>
+        <Link
+          to="/IQAC"
+          className={`menuButton ${location.pathname === '/IQAC' ? 'active' : ''}`}
+        >
+          Director of IQAC
+        </Link>
+        <Link
+          to="/Placements"
+          className={`menuButton ${location.pathname === '/Placements' ? 'active' : ''}`}
+        >
+          Director of IIIT & Placements
+        </Link>
+        <Link
+          to="/Research"
+          className={`menuButton ${location.pathname === '/Research' ? 'active' : ''}`}
+        >
+          Director of Research & Development
+        </Link>
+        <Link
+          to="/SportsAdministration"
+          className={`menuButton ${location.pathname === '/SportsAdministration' ? 'active' : ''}`}
+        >
+          Director of University Games & Sports Administration
+        </Link>
+        
+      </div>
+      <div className="allRightContent">
+            <div className="allRightContentHeading">
+                Director of Research & Development
+            </div>
+                <div class="allRightContentProfile">
+                    <center>
+                    <div class="allRightContentHolderContentProfileImage">
+                        <img src="https://jntugv.edu.in/images/9266a0f9fc226ffbf57a3beb4c6b6388/profilephotos/da.jpg" alt="Profile" />
+                    </div>
+                    <div class="allRightContentHolderContentProfileName">
+                        <h3>Dr. K. Babulu</h3>
+                    </div>
+                    <div class="allRightContentHolderContentProfileDesignation">
+                        <h4 class="regularStyleH4">Director of Research & Development</h4>
+                    </div>
+                    <div class="allRightContentHolderContentProfileQualification">
+                        <span>Professor of  Dept. of Electronics and Communications Engineering</span>
+                    </div>
+                    </center>
+                    
+                <hr/>
+                <p>Email: dr@jntugv.edu.in</p>
+                <hr/>
+                <p className='a'>Educational Qualifications:</p>
+                <p>❖	B.E from Andhra University with Electronics and Communication Engineering.</p>
+                <p>❖	M.Tech from REC, Warangal with Electronic Instrumentation.</p>
+                <p>❖	Ph.D from J.N.T. University, Anantapur in the area of VLSI& Embedded System Design.</p>
+                <hr/>
+                <p className='a'>Industrial Experience:</p>
+                <p>❖	“Engineer Trainee” in Vijaya Electronics Pvt.Ltd, Hyderabad for a period of 1year and 6 months.</p>
+                <p>❖	“Instrumentation Engineer” in Andhra Paper Mills Ltd. Rajahmundry for a period of 6 Months.</p>
+                <hr/>
+                <p className='a'>Teaching Experience:</p>
+                <p>❖	 “Lecturer in the Department of ECE”, at NBKR Institute of Science & Technology, Vidyanagar for a Period of 1 year.</p>
+                <p>❖	 “Assistant Professor in the Department of ECE”,  at NBKR Institute of Science & Technology, Vidyanagar for a period of 2 years and 6 months.</p>
+                <p>❖	 “Associate Professor in the Department of ECE”, at NBKR Institute of Science & Technology, Vidyanagar for a period of 3 years.</p>
+                <p>❖		 “Associate Professor in the Department of ECE”, at JNTU College of Engineering, Anantapur for a period of 4 years</p>
+                <p>❖	 “Associate Professor in the Department of ECE”, at University College of Engineering, JNTUK, Kakinada for a period of 3 years and 5 months.</p>
+                <p>❖	“Professor in the Department of ECE”, at University College of Engineering, JNTUK, Kakinada for a period of 9 years and 11 months.</p>
+                <p>❖	 “Professor in the Department of ECE”, at University College of Engineering, JNTUK, Vizianagaram for a period of 1 year and 3 months.</p>
+                <p>❖	Working as “Professor in the Department of ECE”, at JNTU-GV College of Engineering (CEV), Vizianagaram since January 2022.</p>
+                <hr/>
+                <p className='a'>Memberships:</p>
+                <p>❖	 Life Member of ISTE.</p>
+                <p>❖	 Fellow of IETE (F235351).</p>
+                <p>❖	 Fellow of IE (F1184004).</p>
+                <hr/>
+                <p className='a'>Administrative Responsibilities:</p>
+                <p>❖	  Director, Director Research & Development and Admissions (i/c), JNTU-GV, Vizianagaram.</p>
+                <p>❖	  Director, Library Science & Entrepreneurs Development Centre, JNTUK, Kakinada.</p>
+                <p>❖	  Controller of Examinations at JNTUK, Kakinada.</p>
+                <p>❖	   BOS Chairman for ECE Department at University College of Engineering (Autonomous), JNTUK, Kakinada during 2014-2015.</p>
+                <p>❖	   Head of ECE Department at University College of Engineering, JNTUK, Kakinada.</p>
+                <p>❖	    Procurement Nodal Officer for TEQIP-II at University College of Engineering, JNTUK, Kakinada.</p>
+                <p>❖	     BOS Member for JNTUK, Kakinada, JNTUA, Anantapur and SVUCE, Tirupati.</p>
+                <p>❖	     Taskforce Committee Member for Private Engineering Colleges in Ranga Reddy District, Andhra Pradesh.</p>
+                <p>❖	  Additional controller of Examinations at JNTUK, Kakinada</p>
+                <p>❖	 Convener and Member for Fact Finding Committees of JNTUA, Anatapur, JNTUK, Kakinada and JNTU-GV, Vizianagaram.</p>
+                <p>❖	  In-Charge Head of ECE Department at JNTU College of Engineering, Anantapur.</p>
+                <p>❖	  Staff Coordinator for IEEE Student branch at JNTU College of Engineering, Anantapur.</p>
+                <p>❖	 Student Union Coordinator at JNTU College of Engineering, Anantapur.</p>
+                <p>❖	 Executive Member for ISTE Student chapter at NBKR Institute of Science & Technology, Vidyanagar.</p>
+                <hr/>
+                <p className='a'>List of Publications:</p>
+                <p>❖	 International Journals: 47.</p>
+                <p>❖	 National Journals: 02</p>
+                <p>❖	 International Conferences : 31</p>
+                <p>❖	 National Conferences : 14</p>
+                <hr/>
+                <p className='a'>Ph.D s:</p>
+                <p>❖	 Number of Ph.D s Awarded: 05.</p>
+                <p>❖	 Number of Ph.D Scholars Pursing: 08.</p>
+                <hr/>
+                <p className='a'>Others:</p>
+                <p>❖	 AWARDS: 05.</p>
+                <p>❖	 SHORT TERM TRAINING PROGRAMS (STTP) ATTEND: 03.</p>
+                <p>❖	 WORKSHOPS ATTENED: 11.</p>
+                <p>❖	 REFRESHER COURSE/TRAINING PROGRAMMES ATTENED: 04.</p>
+                <p>❖	 CONFERENCES/TRAINING PROGRAMMES/ WORKSHOPS CONDUCTED: 11.</p>
+                <p>❖	 RESOURCE PERSON: 10.</p>
+                <p>❖	 CHAIR PERSON: 01.</p>
+                <hr/>
+            </div>
+        </div>
+    </div>
+
+
+);
+}
+export default Research;
