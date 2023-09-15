f7a955e45ef88331d190c1507ec54e6ead7cdc23
SUMASRI BHOGI <sumasribhogi.project@gmail.com>
2023-09-15 11:00:19 +0530

    Completed the Administration(Chancellor, Vice Chancellor, Regustrar) and Directorates(Empowerment of Women and grievances,Evaluation,faculty Development cell,Foreign University Relations) part and used the same css for all.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
+import React from 'react';
 import './App.css';
+import Homepage from './Homepage';
 import BelowBar from './components/BelowBar/BelowBar';
-import Footer from './components/MainPage/Footer/Footer';
 import Header from './components/MainPage/Header/Header';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import Policies from './components/MainPage/PoliciesSection/Policies';
-import Notifications from './components/Notifications/Notifications';
 import ReadMore from './components/MainPage/NoticeBoard/ReadMore';
-import Noticeboard from './components/MainPage/NoticeBoard/Noticeboard';
-import Map from './components/MainPage/Map/Map'
-import  { Route,Routes } from 'react-router-dom';
+import Chancellor from './components/MainPage/Header/Administration/Chancellor'; 
+import ViceChancellor from './components/MainPage/Header/Administration/ViceChancellor';
+import Registrar from './components/MainPage/Header/Administration/Registrar';
+import Empowerment from './components/MainPage/Header/Directorates/Empowerment';
+import Evaluation from './components/MainPage/Header/Directorates/Evaluation';
+import FacultyDevelopmentCell from './components/MainPage/Header/Directorates/FacultyDevelopmentCell';
+import ForeignUniversityRelations from './components/MainPage/Header/Directorates/ForeignUniversityRelations';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/MainPage/Footer/Footer';
 
 function App() {
   return (
     <div>
-    <Routes>
-        <Route path="/readmore" element={<ReadMore />}/>
+      <Routes>
+        <Route path="/readmore" element={<ReadMore />} />
       </Routes>
       <BelowBar />
       <Header />
-      <br/>
-      <div className="horizontal-container">
-      <Notifications />
-      <ImageGallery />
-        <Policies />
-        
-      </div>
-      <Noticeboard/>
-      <br/>
-      
-      <Map />
+      <Routes>
+      <Route path="/" element={<Homepage />} /> {/* Add this route */}
+      <Route path="/chancellor" element={<Chancellor />} /> {/* Add this route */}
+      <Route path="/ViceChancellor" element={<ViceChancellor />} /> 
+      <Route path="/Registrar" element={<Registrar />} /> 
+      <Route path="/Empowerment" element={<Empowerment />} />
+      <Route path="/Evaluation" element={<Evaluation />} />  
+      <Route path="/FacultyDevelopmentCell" element={<FacultyDevelopmentCell />} /> 
+      <Route path="/ForeignUniversityRelations" element={<ForeignUniversityRelations />} /> 
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/MainPage/Header/Dropdown.js
+++ src/components/MainPage/Header/Dropdown.js
@@ -4,10 +4,10 @@ const Dropdown = () => {
   return (
     <div class="topnav" id="myTopnav">
       <div class="dropdown">
-        <button class="dropbtn">Home</button>
+        <button class="dropbtn">HOME</button>
       </div>
       <div class="dropdown">
-        <button class="dropbtn">ABOUTUS</button>
+        <button class="dropbtn">ABOUT US</button>
         <div class="dropdown-content">
           <button>
             {" "}
@@ -41,19 +41,19 @@ const Dropdown = () => {
         </div>
       </div>
       <div class="dropdown">
-        <button class="dropbtn">ADIMINSTARATION</button>
+        <button class="dropbtn">ADMINISTRATION</button>
         <div class="dropdown-content">
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 1</Link>
+            <Link className="link-btn" to="/chancellor" target="_self">CHANCELLOR</Link>
           </button>
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 2</Link>
+            <Link className="link-btn" to="/ViceChancellor" target="_self">VICE CHANCELLOR</Link>
           </button>
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 3</Link>
+            <Link className="link-btn" to="/Registrar" target="_self">REGISTRAR</Link>
           </button>
         </div>
       </div>
@@ -62,15 +62,19 @@ const Dropdown = () => {
         <div class="dropdown-content">
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 1</Link>
+            <Link className="link-btn" to="/Empowerment" target="_self">Empowerment of Women & Grievances</Link>
           </button>
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 2</Link>
+            <Link className="link-btn" to="/Evaluation" target="_self">Evaluation</Link>
           </button>
           <button>
             {" "}
-            <Link className="link-btn" to="/Main">Link 3</Link>
+            <Link className="link-btn" to="/FacultyDevelopmentCell" target="_self">Faculty Development Cell</Link>
+          </button>
+          <button>
+            {" "}
+            <Link className="link-btn" to="/ForeignUniversityRelations" target="_self">Foreign University Relations</Link>
           </button>
         </div>
       </div>
