403a0d5291470a0444f17f42373dd0540e0cc2f9
kirtanauppala61 <143117705+kirtanauppala61@users.noreply.github.com>
2023-09-20 17:28:48 +0530

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Footer from "./components/MainPage/Footer/Footer";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Noticeboard from "./components/MainPage/NoticeBoard/Noticeboard";
 import Map from "./components/MainPage/Map/Map";
@@ -12,7 +11,6 @@ function Homepage() {
       <Noticeboard />
       <br />
       <Map />
-      <Footer />
     </div>
   );
 }
