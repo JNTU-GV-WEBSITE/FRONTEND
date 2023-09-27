c024fa04e270078e006802b9bdd4b985e9ca7a46
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-09-27 19:39:07 +0530

    added child routing

--- src/components/MainPage/Header/Academics/Constituent Colleges/ConstituentCollegesOP.js
+++ src/components/MainPage/Header/Academics/Constituent Colleges/ConstituentCollegesOP.js
@@ -5,28 +5,24 @@ const ConstituentCollegesOP = () => {
       <div className="allRightContentAOP">
         <div className="clgheaddiv">Constituent Colleges</div>
         <br></br>
-        <div className="clgdiv">
-          <a
-            className="clgnamediv"
-            href="https://jntugvcev.edu.in/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="clgtextdivOP">
-              JNTU-GV College of Engineering, Vizianagaram
-            </div>
-          </a>
-          <a
-            className="clgnamediv"
-            href="https://jntugvcev.edu.in/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="clgtextdivOP">
-              JNTU-GV Trible College of Engineering, Kurupam
-            </div>
-          </a>
-        </div>
+          <div className="clgdiv">
+            <a
+              className="clgnamediv"
+              href="https://jntugvcev.edu.in/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="clgtextdivOP">JNTU-GV College of Engineering, Vizianagaram</div>
+            </a>
+            <a
+               className="clgnamediv"
+               href="https://jntugvcev.edu.in/"
+               target="_blank"
+               rel="noreferrer"
+            >
+              <div className="clgtextdivOP">JNTU-GV Trible College of Engineering, Kurupam</div>
+            </a>
+          </div>
       </div>
   );
 };
