326375524f194b2de5579455987a9321ac62bf39
ADARSHNALLA41 <adarshnalla.project@gmail.com>
2023-10-17 22:10:48 +0530

    updated News and Events Data_2

--- src/components/NewsAndEvents/Scrollbar.js
+++ src/components/NewsAndEvents/Scrollbar.js
@@ -10,10 +10,10 @@ const arr = [
     { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1696406940-1250AC for I Year M.pdf", text: "Academic Calendar for I Year M. Tech and M. Pharmacy " },
     { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695812280-34056APSCHE UNICEF Launched P2E Program.pdf", text: "APSCHE-BCDE-UNICEF-P2E Program " },
     { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695790980-78550Academic Calander for II year B.pdf", text: "Academic Calender for II year B. Pharmacy AY 2023-24 " },
-    { text: "Ph.D-Tution Fee-Application.", hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695715440-1649125-09-2023-Ph.pdf" },
-    { text: "Ph.D-Tution Fee-Circular.", hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695715260-5663525-09-2023-Ph.pdf" },
-    { text: "IEEE Vizag Bay COMSOC/SPS Distinguished Lecture by Dr.Sudhir Dixit, San\u00a0Francisco,\u00a0USA.", hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695449340-12470Distinguished Lecture on 29 Sep' 2023.pdf" },
-    { text: "Most Urgent: Appointment of Student Grievances Redressal Committee (SGRC) and Ombud Persons in Constituent and Affiliated Colleges",hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695383340-65872sgrc.pdf"}
+    { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695715440-1649125-09-2023-Ph.pdf", text: "Ph.D-Tution Fee-Application. "},
+    { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695715260-5663525-09-2023-Ph.pdf", text: "Ph.D-Tution Fee-Circular. "},
+    { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695449340-12470Distinguished Lecture on 29 Sep' 2023.pdf", text: "IEEE Vizag Bay COMSOC/SPS Distinguished Lecture by Dr.Sudhir Dixit, San\u00a0Francisco,\u00a0USA. "},
+    { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695383340-65872sgrc.pdf", text: "Most Urgent: Appointment of Student Grievances Redressal Committee (SGRC) and Ombud Persons in Constituent and Affiliated Colleges "}
 ]  
 
 function Scrollbar()
