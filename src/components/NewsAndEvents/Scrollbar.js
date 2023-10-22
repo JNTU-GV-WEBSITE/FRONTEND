9e7fe79df7f428fe5946b96ce2c2b764b663f730
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-10-22 16:04:04 +0530

    added the latest news and events

--- src/components/NewsAndEvents/Scrollbar.js
+++ src/components/NewsAndEvents/Scrollbar.js
@@ -1,54 +1,101 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useEffect, useRef } from 'react';
-import $ from 'jquery';
-import  'jquery.marquee';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useRef } from "react";
+import $ from "jquery";
+import "jquery.marquee";
 import "./NewsAndEvents.css";
-import CampaignIcon from '@mui/icons-material/Campaign';
+import CampaignIcon from "@mui/icons-material/Campaign";
 
 const arr = [
-    { hypertext: "https://drive.google.com/file/d/12h7w_EuGdnR3gqAEyM68HcLmUIx26qaI/view?usp=share_link", text: "Academic Calendar for I Year MBA and MCA " },
-    { hypertext: "https://drive.google.com/file/d/18SIawj2DntYndNAsjXLA7-3s1f-23G9q/view?usp=share_link", text: "Academic Calendar for I Year M. Tech and M. Pharmacy " },
-    { hypertext: "https://jntugv.edu.in/updates/9266a0f9fc226ffbf57a3beb4c6b6388/notifications/1695812280-34056APSCHE UNICEF Launched P2E Program.pdf", text: "APSCHE-BCDE-UNICEF-P2E Program " },
-    { hypertext: "https://drive.google.com/file/d/1IrawmiHcz5rP2le0Q5pXrNyt75psQnzH/view?usp=sharing", text: "Academic Calender for II year B. Pharmacy AY 2023-24 " },
-    { hypertext: "https://drive.google.com/file/d/1rCCC2qpgSXno7O-8qpwcr6SUfFHxGDtz/view?usp=sharing", text: "Ph.D-Tution Fee-Application. "},
-    { hypertext: "https://drive.google.com/file/d/1tVcX10y17yM0nQHx6a06zkrtQtItclWZ/view?usp=sharing", text: "Ph.D-Tution Fee-Circular. "},
-    { hypertext: "https://drive.google.com/file/d/1AGieJ4z5hizI-rPQXyuvgy2pucOFHpRt/view?usp=sharing", text: "IEEE Vizag Bay COMSOC/SPS Distinguished Lecture by Dr.Sudhir Dixit, San\u00a0Francisco,\u00a0USA. "},
-    { hypertext: "https://drive.google.com/file/d/164BCaHh39X0JwmkRoQ5HqVUMfqZFGaHV/view?usp=sharing", text: "Most Urgent: Appointment of Student Grievances Redressal Committee (SGRC) and Ombud Persons in Constituent and Affiliated Colleges "}
-]  
+  {
+    hypertext:
+      "https://drive.google.com/file/d/18rWsMfC8RGnEugCcDhP3h5wnpj4sxS4o/view?usp=share_link",
+    text: "IV B.Tech - I Sem Reg & Supple Exams Notification-December-2023",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1dqy_rgi86_u9CMydWSrV50LO65Ddy4Xz/view?usp=share_link",
+    text: "IV B.Pharmacy - I Sem Reg & Supple Exams Notification-December-2023",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1kEWYzYKvIU7mupDJ37AJARETVZ1aD3vN/view?usp=share_link",
+    text: "List of Examiners for Spot Valuation of MBA- IV Semester Regular & Supply-July-2023",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1KTU0LGyTzSQN6SwKRTiUoCC8HYkxP56H/view?usp=share_link",
+    text: "Academic Calander for III Pharm D Academic Year 2023-24",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1C98jPJyDk3AY9ZgcZ0l2YCfmxzBIq9y5/view?usp=share_link",
+    text: "Academic Calander for II Pharm D Academic Year 2023-24",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1kEWYzYKvIU7mupDJ37AJARETVZ1aD3vN/view?usp=share_link",
+    text: "MBA -III Sem Reg & Supply Exams Notification,Dec 2023",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1yp_Kqj8nPwu2hdmUg38SQ-BWkqWwf1u3/view?usp=share_link",
+    text: "MTECH -III Sem Reg & Supply Exams Notification,Dec 2023",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1VSTiSf7kSLO0bszctgAEmxKHN_znYMfS/view?usp=share_link",
+    text: "MCA -III Sem R&S Notification , Dec 2023",
+  },
+];
 
-function Scrollbar()
-{
+function Scrollbar() {
+  const el = useRef();
 
-    const el = useRef();
-
-  useEffect(function() {
+  useEffect(function () {
     const $el = $(el.current);
 
-        $el.marquee({
-        duration: 10000,
-        gap: 50,
-        delayBeforeStart: 0,
-        direction: 'up',
-        pauseOnHover: true
-        });
+    $el.marquee({
+      duration: 10000,
+      gap: 50,
+      delayBeforeStart: 0,
+      direction: "up",
+      pauseOnHover: true,
     });
-    
-    //The Scrollbar component
-    return (
-        <div className="d1">
-            <center><h1 className="h1"><CampaignIcon sx={{fontSize:'45px',transform:'scaleX(-1)'}}></CampaignIcon> NEWS AND EVENTS <CampaignIcon sx={{fontSize:'45px'}}></CampaignIcon></h1></center>
-            <div className="m1" ref={el}>
-                
-                {arr.map((item, i) => (
-                    <div>
-                        <a href={item.hypertext}><div>{item.text}<img src="images/new.gif" alt="newimg" height="20vh" width="50vh"></img><hr /></div></a>
-                    </div>
-                ))}
-                
-            </div>
-            <button className="btn w-100">Show All...</button>
-        </div>
-    );
+  });
+
+  //The Scrollbar component
+  return (
+    <div className="d1">
+      <center>
+        <h1 className="h1">
+          <CampaignIcon
+            sx={{ fontSize: "45px", transform: "scaleX(-1)" }}
+          ></CampaignIcon>{" "}
+          NEWS AND EVENTS{" "}
+          <CampaignIcon sx={{ fontSize: "45px" }}></CampaignIcon>
+        </h1>
+      </center>
+      <div className="m1" ref={el}>
+        {arr.map((item, i) => (
+          <div>
+            <a href={item.hypertext}>
+              <div>
+                {item.text}
+                <img
+                  src="images/new.gif"
+                  alt="newimg"
+                  height="20vh"
+                  width="50vh"
+                ></img>
+                <hr />
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+      <button className="btn w-100">Show All...</button>
+    </div>
+  );
 }
 
 export default Scrollbar;
