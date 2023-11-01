005712bb0783bd2f3e8e97b3b4d3851d3553c40e
haneeshbandaru20 <haneeshbandaru.project@gmail.com>
2023-11-01 18:05:50 +0530

    added new notification

--- src/components/GrievanceForm/GrievanceForm.js
+++ src/components/GrievanceForm/GrievanceForm.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import axios from 'axios';
 import "./Grievance.css";
 
-const InputField = ({ label, type, value, onChange, required = false }) => (
+const InputField = ({ label, type, name, value, onChange, required = false }) => (
   <div className="form-floating mb-3">
     <input
       type={type}
       className="form-control"
       placeholder={label}
+      name={name}
       value={value}
       onChange={onChange}
       required={required}
@@ -19,7 +20,7 @@ const InputField = ({ label, type, value, onChange, required = false }) => (
 );
 
 const Grievance = () => {
-  const [formData, setFormData] = useState({
+  const initialState = {
     rollno: "",
     email: "",
     name: "",
@@ -30,9 +31,9 @@ const Grievance = () => {
     msg: "",
     date: "",
     file: null,
-  });
+  };
 
-  const { rollno, email, name, phno, adhaarno, collegename, category, msg, date, file } = formData;
+  const [formData, setFormData] = useState(initialState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -80,20 +81,20 @@ const Grievance = () => {
         <div style={{ color: "red" }}>* denotes a required field</div>
       </div>
       <form>
-        <InputField label="Student Roll Number" type="text" name="rollno" value={rollno} onChange={handleChange} required />
-        <InputField label="Email" type="email" name="email" value={email} onChange={handleChange} required />
-        <InputField label="Full Name" type="text" name="name" value={name} onChange={handleChange} required />
-        <InputField label="Aadhar Number" type="number" name="adhaarno" value={adhaarno} onChange={handleChange} required />
-        <InputField label="College Name" type="text" name="collegename" value={collegename} onChange={handleChange} required />
-        <InputField label="Phone Number" type="number" name="phno" value={phno} onChange={handleChange} required />
-        <InputField label="Category of Grievance" type="text" name="category" value={category} onChange={handleChange} required />
+        <InputField label="Student Roll Number" type="text" name="rollno" value={formData.rollno} onChange={handleChange} required />
+        <InputField label="Email" type="email" name="email" value={formData.email} onChange={handleChange} required />
+        <InputField label="Full Name" type="text" name="name" value={formData.name} onChange={handleChange} required />
+        <InputField label="Aadhar Number" type="number" name="adhaarno" value={formData.adhaarno} onChange={handleChange} required />
+        <InputField label="College Name" type="text" name="collegename" value={formData.collegename} onChange={handleChange} required />
+        <InputField label="Phone Number" type="number" name="phno" value={formData.phno} onChange={handleChange} required />
+        <InputField label="Category of Grievance" type="text" name="category" value={formData.category} onChange={handleChange} required />
         <div className="form-floating mb-3">
           <textarea
             style={{ height: "22vh" }}
             className="form-control"
             placeholder="Detailed Description of the Grievance"
             name="msg"
-            value={msg}
+            value={formData.msg}
             onChange={handleChange}
             required
           />
@@ -101,7 +102,7 @@ const Grievance = () => {
             Detailed Description of the Grievance <span className="required">*</span>
           </label>
         </div>
-        <InputField label="Date of Incident" type="date" name="date" value={date} onChange={handleChange} required />
+        <InputField label="Date of Incident" type="date" name="date" value={formData.date} onChange={handleChange} required />
         <div className="mb-3">
           <label className="form-label">
             Any Supporting Documents (Attachment) <span className="required">*</span>
