71bc96d481dfe930a71df0668aa6afca75450558
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-11-30 13:21:29 +0000

    added examinations

--- src/components/NewsAndEvents/ScrollData.js
+++ src/components/NewsAndEvents/ScrollData.js
@@ -1,6 +1,27 @@
 export const arr = [
   {
     hypertext:
+      "https://drive.google.com/file/d/1vD1BH9VNxFZ3660Tq9TYUWAp8fHMhrXE/view?usp=sharing",
+    text: "Limited notification for SC/ST Backlog Vacancies",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1ZVrwi5iApIEZr5-2eg9MAeXgijEBWcKp/view?usp=sharing",
+    text: `Notification for Assistant Professors and Academic Non-Vacation Positions
+      (BC Backlog and Regular Vacancies)`,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1BqL8z-1_G_bR-9qjz5SQrF3ItE-nnMdw/view?usp=sharing",
+    text: "JNTUGV-Detailed Notification for Associate Professors",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1LkPzCEEBGsK6xVvva3uXtDGEv9hEmHu1/view?usp=sharing",
+    text: "JNTUGV-Detailed Notification for Professors",
+  },
+  {
+    hypertext:
       "https://drive.google.com/file/d/18rWsMfC8RGnEugCcDhP3h5wnpj4sxS4o/view?usp=share_link",
     text: "IV B.Tech - I Sem Reg & Supple Exams Notification-December-2023",
   },
@@ -18,30 +39,30 @@ export const arr = [
     hypertext:
       "https://drive.google.com/file/d/1KTU0LGyTzSQN6SwKRTiUoCC8HYkxP56H/view?usp=share_link",
     text: "Academic Calander for III Pharm D Academic Year 2023-24",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1C98jPJyDk3AY9ZgcZ0l2YCfmxzBIq9y5/view?usp=share_link",
-    text: "Academic Calander for II Pharm D Academic Year 2023-24",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1kEWYzYKvIU7mupDJ37AJARETVZ1aD3vN/view?usp=share_link",
-    text: "MBA -III Sem Reg & Supply Exams Notification,Dec 2023",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1yp_Kqj8nPwu2hdmUg38SQ-BWkqWwf1u3/view?usp=share_link",
-    text: "MTECH -III Sem Reg & Supply Exams Notification,Dec 2023",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1VSTiSf7kSLO0bszctgAEmxKHN_znYMfS/view?usp=share_link",
-    text: "MCA -III Sem R&S Notification , Dec 2023",
-  },
+  }
 ];
 
 export const newNotification = [
+  {
+    hypertext:
+      "https://drive.google.com/file/d/11xMjbXC8QFDedoJ8cKLP6YS2fh0P323I/view?usp=sharing",
+    text: `IV B.Tech I Semester Jumbling centers `,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1vIXgi5138lBTyPm1rIR-kX36Tbll8u9x/view?usp=sharing",
+    text: `IV-I B.Tech R16 Supple-Time Table-Dec-2023 `,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1QWnLi30dkTFsGk2xRtm5Un_G1Fy7wI-d/view?usp=sharing",
+    text: `IV-I B.Tech R19 Supple-Time Table-Dec-2023 `,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/19fQCUNwdVYZ96kRgH7q12H3uz2nNoZXz/view?usp=sharing",
+    text: `Revised IV-I B.Tech R20 Regular-Time Table-Dec-2023`,
+  },
   {
     hypertext:
       "https://drive.google.com/file/d/1vDeOmbk83fv-6jHVOrkn3tXL4a6FN5Zt/view?usp=sharing",
@@ -108,25 +129,5 @@ export const newNotification = [
     text: "RRM-Application Format",
   },
 
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1vD1BH9VNxFZ3660Tq9TYUWAp8fHMhrXE/view?usp=sharing",
-    text: "Limited notification for SC/ST Backlog Vacancies",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1ZVrwi5iApIEZr5-2eg9MAeXgijEBWcKp/view?usp=sharing",
-    text: `Notification for Assistant Professors and Academic Non-Vacation Positions
-      (BC Backlog and Regular Vacancies)`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1BqL8z-1_G_bR-9qjz5SQrF3ItE-nnMdw/view?usp=sharing",
-    text: "JNTUGV-Detailed Notification for Associate Professors",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1LkPzCEEBGsK6xVvva3uXtDGEv9hEmHu1/view?usp=sharing",
-    text: "JNTUGV-Detailed Notification for Professors",
-  },
+
 ];
