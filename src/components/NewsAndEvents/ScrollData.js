187029109f6bc32aae34cd3620aa083b2e2e54b5
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-11-30 13:22:29 +0000

    added examinations

--- src/components/NewsAndEvents/ScrollData.js
+++ src/components/NewsAndEvents/ScrollData.js
@@ -1,6 +1,48 @@
 export const arr = [
   {
     hypertext:
+      "https://drive.google.com/file/d/10-uVSgm0HRa8xxPliD4FM7FdelctQQuE/view?usp=sharing",
+    text: `Yoga (M & W) Team Selection Trails`,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1c5cvQ1T45hcmFNE7lntQ5DFZPt8bpdRY/view?usp=sharing",
+    text: `III B.Tech ISem RegularSupply R20, R19, R16 Notification`,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/15GSrtZ6Sq8-mbAad6u0wlr6xu98PdILV/view?usp=sharing",
+    text: `Up-Skilling Program In "IoT Technologies" In Collaboration With C-DAC and IEEE and CSC Academy /CSC e-Governance Services India Limited.
+    (check workshops tab for more details)`,
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1XvQrUyp63GuYrSgFP_Bd4a7u28EEVAyl/view?usp=sharing",
+    text: "JNTUGV Inter Collegiate Tournaments Dates & Venues 2023-24",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1esNxbq-QNf_DouXxG9wz0pGV6RZsBMCq/view?usp=sharing",
+    text: "DR&D-Circular-Research Methodology",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1YAP2ckGybUcgf7Q_h4Mpr84Mx-silUwV/view?usp=sharing",
+    text: "Circular (For New NSS Units)",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1SXtfk8ZVcFUZr9wHGHquhwNBiHtuBsZg/view?usp=sharing",
+    text: "RRM Notification for AY 2019-20 & Ealier Batches",
+  },
+  {
+    hypertext:
+      "https://drive.google.com/file/d/1MkePk2qr6fExE33Wp1L8SwMH5q9NtEaq/view?usp=sharing",
+    text: "RRM-Application Format",
+  },
+
+  {
+    hypertext:
       "https://drive.google.com/file/d/1vD1BH9VNxFZ3660Tq9TYUWAp8fHMhrXE/view?usp=sharing",
     text: "Limited notification for SC/ST Backlog Vacancies",
   },
@@ -86,48 +128,6 @@ export const newNotification = [
     text: `Academic Calendar for I Year B Pharmacy for the Academic Year 2023-24 `,
   },
 
-
-  {
-    hypertext:
-      "https://drive.google.com/file/d/10-uVSgm0HRa8xxPliD4FM7FdelctQQuE/view?usp=sharing",
-    text: `Yoga (M & W) Team Selection Trails`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1c5cvQ1T45hcmFNE7lntQ5DFZPt8bpdRY/view?usp=sharing",
-    text: `III B.Tech ISem RegularSupply R20, R19, R16 Notification`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/15GSrtZ6Sq8-mbAad6u0wlr6xu98PdILV/view?usp=sharing",
-    text: `Up-Skilling Program In "IoT Technologies" In Collaboration With C-DAC and IEEE and CSC Academy /CSC e-Governance Services India Limited.
-    (check workshops tab for more details)`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1XvQrUyp63GuYrSgFP_Bd4a7u28EEVAyl/view?usp=sharing",
-    text: "JNTUGV Inter Collegiate Tournaments Dates & Venues 2023-24",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1esNxbq-QNf_DouXxG9wz0pGV6RZsBMCq/view?usp=sharing",
-    text: "DR&D-Circular-Research Methodology",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1YAP2ckGybUcgf7Q_h4Mpr84Mx-silUwV/view?usp=sharing",
-    text: "Circular (For New NSS Units)",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1SXtfk8ZVcFUZr9wHGHquhwNBiHtuBsZg/view?usp=sharing",
-    text: "RRM Notification for AY 2019-20 & Ealier Batches",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1MkePk2qr6fExE33Wp1L8SwMH5q9NtEaq/view?usp=sharing",
-    text: "RRM-Application Format",
-  },
 
 
 ];
