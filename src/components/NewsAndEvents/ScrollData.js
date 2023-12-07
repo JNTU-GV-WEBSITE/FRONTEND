da7dc1c041de0aa525fe6a1e205645d947097ec2
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-12-07 15:30:00 +0000

    updates

--- src/components/NewsAndEvents/ScrollData.js
+++ src/components/NewsAndEvents/ScrollData.js
@@ -1,104 +1,15 @@
 export const arr = [
   {
     hypertext:
-      "https://drive.google.com/file/d/10-uVSgm0HRa8xxPliD4FM7FdelctQQuE/view?usp=sharing",
-    text: `Yoga (M & W) Team Selection Trails`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1c5cvQ1T45hcmFNE7lntQ5DFZPt8bpdRY/view?usp=sharing",
-    text: `III B.Tech ISem RegularSupply R20, R19, R16 Notification`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/15GSrtZ6Sq8-mbAad6u0wlr6xu98PdILV/view?usp=sharing",
-    text: `Up-Skilling Program In "IoT Technologies" In Collaboration With C-DAC and IEEE and CSC Academy /CSC e-Governance Services India Limited.
-    (check workshops tab for more details)`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1XvQrUyp63GuYrSgFP_Bd4a7u28EEVAyl/view?usp=sharing",
-    text: "JNTUGV Inter Collegiate Tournaments Dates & Venues 2023-24",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1esNxbq-QNf_DouXxG9wz0pGV6RZsBMCq/view?usp=sharing",
-    text: "DR&D-Circular-Research Methodology",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1YAP2ckGybUcgf7Q_h4Mpr84Mx-silUwV/view?usp=sharing",
-    text: "Circular (For New NSS Units)",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1SXtfk8ZVcFUZr9wHGHquhwNBiHtuBsZg/view?usp=sharing",
-    text: "RRM Notification for AY 2019-20 & Ealier Batches",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1MkePk2qr6fExE33Wp1L8SwMH5q9NtEaq/view?usp=sharing",
-    text: "RRM-Application Format",
-  },
-
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1vD1BH9VNxFZ3660Tq9TYUWAp8fHMhrXE/view?usp=sharing",
-    text: "Limited notification for SC/ST Backlog Vacancies",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1ZVrwi5iApIEZr5-2eg9MAeXgijEBWcKp/view?usp=sharing",
-    text: `Notification for Assistant Professors and Academic Non-Vacation Positions
-      (BC Backlog and Regular Vacancies)`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1BqL8z-1_G_bR-9qjz5SQrF3ItE-nnMdw/view?usp=sharing",
-    text: "JNTUGV-Detailed Notification for Associate Professors",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1LkPzCEEBGsK6xVvva3uXtDGEv9hEmHu1/view?usp=sharing",
-    text: "JNTUGV-Detailed Notification for Professors",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/18rWsMfC8RGnEugCcDhP3h5wnpj4sxS4o/view?usp=share_link",
-    text: "IV B.Tech - I Sem Reg & Supple Exams Notification-December-2023",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1dqy_rgi86_u9CMydWSrV50LO65Ddy4Xz/view?usp=share_link",
-    text: "IV B.Pharmacy - I Sem Reg & Supple Exams Notification-December-2023",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1kEWYzYKvIU7mupDJ37AJARETVZ1aD3vN/view?usp=share_link",
-    text: "List of Examiners for Spot Valuation of MBA- IV Semester Regular & Supply-July-2023",
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1KTU0LGyTzSQN6SwKRTiUoCC8HYkxP56H/view?usp=share_link",
-    text: "Academic Calander for III Pharm D Academic Year 2023-24",
-  }
-];
-
-export const newNotification = [
-
-  {
-    hypertext:
       "https://drive.google.com/file/d/1MoCjsG1Fb_bLTgt-tZxuEzFDOMRHm0gQ/view?usp=sharing",
     text: `JNTUGV-DAA-UGC 2(f), 12(B) and Autonomous Status-Change of Affiliation from JNTUK, Kakinada to JNTUGV, Vizianagaram `,
   },
   {
-
-
     hypertext:
       "#",
     text: `It is to inform to all affiliated colleges Principals 06-12-2023 all external examinations are postponed due to cyclone.Exam date will be intimated later
-  Regards
-  Director of Evaluation`,
+      Regards
+      Director of Evaluation`,
   },
   {
     hypertext:
@@ -125,30 +36,32 @@ export const newNotification = [
     hypertext:
       "https://drive.google.com/file/d/19fQCUNwdVYZ96kRgH7q12H3uz2nNoZXz/view?usp=sharing",
     text: `Revised IV-I B.Tech R20 Regular-Time Table-Dec-2023`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1vDeOmbk83fv-6jHVOrkn3tXL4a6FN5Zt/view?usp=sharing",
-    text: `Volley Ball (Women) Team Selected Players List`,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1X8FfvLe3QNG9VRrTAgElp3kZZ5aZoxX8/view?usp=sharing",
-    text: `Weightlifting (Men & Women) Team Selected Players List`,
-  },
+  }
 
+];
 
+export const newNotification = [
   {
     hypertext:
-      "https://drive.google.com/file/d/1WP--NiUYNNfPR1nJrqGR5bOEQsP84s7b/view?usp=sharing",
-    text: `Academic Calendar for I Year Pharma D for the Academic Year 2023-24`,
-  },
-  {
+      "https://forms.gle/W3PwBzhPJvqB7QiS7",
+    text: `A 5-Day International Short Term Training Program(STTP) on "Cyber Security: Browser Security`,
+  }, {
     hypertext:
-      "https://drive.google.com/file/d/19Zkp0k64ouvlYw2awLs7IY408OqsZcbA/view?usp=sharing",
-    text: `Academic Calendar for I Year B Pharmacy for the Academic Year 2023-24 `,
+      "https://drive.google.com/file/d/1t5qIU8dMnK1XMoZrYBG6X1Y6WLmDj05d/view?usp=sharing",
+    text: `JNTUGV-DAA- "Certificate Verification " - Admissions- for UG (B.Tech Courses) for the Academic Year 2023-2024`,
+  }, {
+    hypertext:
+      "https://drive.google.com/file/d/16EPIsRX3jeRbI2e-MZMaV9O6pG_-aOF9/view?usp=sharing",
+    text: `JNTUGV_3-1-R20 Supple TIME TABLE December 2023`,
+  }, {
+    hypertext:
+      "https://drive.google.com/file/d/1Tp8iVEUmHyTjRvKleICh8M9TBC5Yjnox/view?usp=sharing",
+    text: `JNTUGV_3-1-R19 Supple TIME TABLE December 2023`,
+  }, {
+    hypertext:
+      "https://drive.google.com/file/d/1_B3cciESIHVULCYeKBNz-NqxKBsPIQrO/view?usp=sharing",
+    text: `JNTUGV_3-1-R16-Supple- TIME TABLE December 2023`,
   },
-
 
 
 ];
