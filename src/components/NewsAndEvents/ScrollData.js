80e5bc4b8c8e1e2a028b0303ef3ff15060c83c91
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-12-16 13:37:53 +0530

    Update ScrollData.js

--- src/components/NewsAndEvents/ScrollData.js
+++ src/components/NewsAndEvents/ScrollData.js
@@ -1,4 +1,16 @@
-export const arr = [
+export const arr = [{
+  hypertext:
+  "https://drive.google.com/file/d/16EPIsRX3jeRbI2e-MZMaV9O6pG_-aOF9/view?usp=sharing",
+    text: `JNTUGV_3-1-R20 Supple TIME TABLE December 2023`,
+  }, {
+  hypertext:
+  "https://drive.google.com/file/d/1Tp8iVEUmHyTjRvKleICh8M9TBC5Yjnox/view?usp=sharing",
+    text: `JNTUGV_3-1-R19 Supple TIME TABLE December 2023`,
+  }, {
+  hypertext:
+  "https://drive.google.com/file/d/1_B3cciESIHVULCYeKBNz-NqxKBsPIQrO/view?usp=sharing",
+    text: `JNTUGV_3-1-R16-Supple- TIME TABLE December 2023`,
+  },
   {
     hypertext:
       "https://drive.google.com/file/d/1MoCjsG1Fb_bLTgt-tZxuEzFDOMRHm0gQ/view?usp=sharing",
@@ -11,32 +23,6 @@ export const arr = [
       Regards
       Director of Evaluation`,
   },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/19nwWCmz3G2yvGXKB5klf7ZmVcAr_iN_o/view?usp=sharing",
-    text: `Circular -Holiday-Cyclone Michuang- 5-12-2023 `,
-  },
-
-  {
-    hypertext:
-      "https://drive.google.com/file/d/11xMjbXC8QFDedoJ8cKLP6YS2fh0P323I/view?usp=sharing",
-    text: `IV B.Tech I Semester Jumbling centers `,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1vIXgi5138lBTyPm1rIR-kX36Tbll8u9x/view?usp=sharing",
-    text: `IV-I B.Tech R16 Supple-Time Table-Dec-2023 `,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/1QWnLi30dkTFsGk2xRtm5Un_G1Fy7wI-d/view?usp=sharing",
-    text: `IV-I B.Tech R19 Supple-Time Table-Dec-2023 `,
-  },
-  {
-    hypertext:
-      "https://drive.google.com/file/d/19fQCUNwdVYZ96kRgH7q12H3uz2nNoZXz/view?usp=sharing",
-    text: `Revised IV-I B.Tech R20 Regular-Time Table-Dec-2023`,
-  }
 
 ];
 
@@ -82,19 +68,7 @@ export const newNotification = [
   hypertext:
   "https://drive.google.com/file/d/1t5qIU8dMnK1XMoZrYBG6X1Y6WLmDj05d/view?usp=sharing",
     text: `JNTUGV-DAA- "Certificate Verification " - Admissions- for UG (B.Tech Courses) for the Academic Year 2023-2024`,
-  }, {
-  hypertext:
-  "https://drive.google.com/file/d/16EPIsRX3jeRbI2e-MZMaV9O6pG_-aOF9/view?usp=sharing",
-    text: `JNTUGV_3-1-R20 Supple TIME TABLE December 2023`,
-  }, {
-  hypertext:
-  "https://drive.google.com/file/d/1Tp8iVEUmHyTjRvKleICh8M9TBC5Yjnox/view?usp=sharing",
-    text: `JNTUGV_3-1-R19 Supple TIME TABLE December 2023`,
-  }, {
-  hypertext:
-  "https://drive.google.com/file/d/1_B3cciESIHVULCYeKBNz-NqxKBsPIQrO/view?usp=sharing",
-    text: `JNTUGV_3-1-R16-Supple- TIME TABLE December 2023`,
-  },
+  }, 
 
 
 ];
