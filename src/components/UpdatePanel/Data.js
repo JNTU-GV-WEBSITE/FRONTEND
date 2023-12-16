9dd1c1b72ce3ac3566de6db4feaa920c5b8835d0
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-12-16 13:33:27 +0530

    Update Data.js

--- src/components/UpdatePanel/Data.js
+++ src/components/UpdatePanel/Data.js
@@ -1,6 +1,6 @@
 export const Data = [
   {
-    description: `I B. Tech I Sem Regular and Supplementary Notification`,
+    description: `I B. Tech I Semester Regular and Supplementary Notification`,
     type: "notifications",
     link: "https://drive.google.com/file/d/1klTCR02ufDCCQ3xMRTrEDz5Qik7qWcdP/view?usp=sharing",
     date: "15",
