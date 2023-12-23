4f76f1fb086555d5ffe8159edef0d2e82e14688f
haneeshbandaru20 <144092852+haneeshbandaru20@users.noreply.github.com>
2023-12-23 11:46:56 +0000

    new notification

--- src/components/NewsAndEvents/ScrollData.js
+++ src/components/NewsAndEvents/ScrollData.js
@@ -33,28 +33,16 @@ export const newNotification = [
       "https://drive.google.com/file/d/1V_bfflY-KLQc8gfh5TFuhmwLw5_5HnsM/view?usp=sharing",
     text: `DAA/JNTU-GV-Request to clear outstanding dues of a Affiliation/University common Serives Fee up to Academic Year 2022-2023 to the University`,
   },
-
-
-
   {
     hypertext:
       "https://drive.google.com/file/d/1IU67Uo-41BT6sXZx4iAwL_Ex5bQwEQ3s/view?usp=drivesdk",
     text: `Revised Academic calanders for B. Pharmacy III year II Semester AY 2023-24`,
   },
-
-
   {
     hypertext:
       "https://drive.google.com/file/d/11OXj7YlOOWJC61BwbhOVX2YkvqY4L_5v/view?usp=drivesdk",
     text: `Revised Academic calanders for B. Tech III year II Semester AY 2023-24`,
   },
-
-
-
-
-
-
-
   {
     hypertext:
       "https://drive.google.com/file/d/1klTCR02ufDCCQ3xMRTrEDz5Qik7qWcdP/view?usp=sharing",
@@ -68,10 +56,6 @@ export const newNotification = [
       "https://drive.google.com/file/d/1ORgylAzE_1NJ9lkjOGlhEDghv2vSP6yu/view?usp=sharing",
     text: `Application form for Revaluation`,
   },
-
-
-
-
   {
     hypertext:
       "https://drive.google.com/file/d/1tAzKvlxWNmHMvkynN44Jyck-btPAzRSa/view?usp=sharing",
@@ -81,8 +65,6 @@ export const newNotification = [
       "https://drive.google.com/file/d/1cJrdKJ7GP0HumNrP7M1Iq2YFVRXWu4S5/view?usp=sharing",
     text: `Pre Ph.D. Examination 2023 Notification`,
   },
-
-
   {
     hypertext:
       "https://drive.google.com/file/d/1rWztYnZOlyvx60MRbZKAVXtm-RnLseiC/view?usp=sharing",
@@ -97,6 +79,4 @@ export const newNotification = [
       "https://drive.google.com/file/d/1t5qIU8dMnK1XMoZrYBG6X1Y6WLmDj05d/view?usp=sharing",
     text: `JNTUGV-DAA- "Certificate Verification " - Admissions- for UG (B.Tech Courses) for the Academic Year 2023-2024`,
   },
-
-
 ];
